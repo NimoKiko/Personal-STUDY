b3863bf0d4c279d3d5a7eb3b8257c5a1478d47da
hyf <973528232@qq.com>
2023-06-29 15:03:22 +0800

    接口相关知识点

--- Node/30-使用express写接口.js
+++ Node/30-使用express写接口.js
@@ -6,6 +6,18 @@ const app = express();
 // 注册express.urlencoded()中间件，解析URL-encoded类型数据
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+/**
+ * 使用CORS中间件处理跨域问题
+ * 1.安装 ：npm i cors
+ * 2.导入：require('cors')
+ * 3.注册：app.use(cors())
+ * tips：必须在路由前注册cors中间件
+ */
+const cors = require('cors')
+
+app.use(cors())
+
 // 注册路由模块
 app.use("/api", apiRouter);
 
--- Node/30.1-apiRouter.js
+++ Node/30.1-apiRouter.js
@@ -12,6 +12,17 @@ router.get("/get", (req, res) => {
     msg: "GET SUCCESS", //状态描述
     data: query, // 响应的数据
   });
+  /**
+   * CORS请求头：
+   * - 1.Access-Control-Allow-Origin:指定允许访问资源的外部URL
+   *  res.setHeader('Access-Control-Allow-Origin','xxxx(url)' | '*')
+   * 
+   * - 2.Access-Control-Allow-Headers:声明允许客户端向服务端发送的请求头
+   *  res.setHeader('Access-Control-Allow-Headers','允许的请求头',...)
+   * 
+   * - 3.Access-Control-Allow-Methods:声明允许客户端向服务端发起的请求方式
+   *  res.setHeader('Access-Control-Allow-Methods','请求方式(PUT,DELETE...)')
+   */
 });
 
 router.post("/post", (req, res) => {
@@ -24,3 +35,17 @@ router.post("/post", (req, res) => {
   });
 });
 module.exports = router;
+
+/**
+ * CORS请求的分类:
+ * - 1.简单请求
+ *    - 请求方式必须是GET,POST,HEAD三者之一
+ *    - HTTP的头部信息只有：application/x-www-form-urlencoded、multipart/form-data、text/plain
+ * 
+ * - 2.预检请求(只要符合以下任一条件都属于预检请求)
+ *    - 请求方式为GET、POST、HEAD之外的Method类型
+ *    - 请求头中包含了自定义的头部请求
+ *    - 向服务器发送了application/json格式的数据
+ * 在浏览器与服务器正式通讯之前，浏览器会先发送OPTION请求进行预检，以获知服务器是否允许该实际请求。
+ * 服务器发送了成功响应请求后才会发送真正的请求，且携带真实数据。
+ */
